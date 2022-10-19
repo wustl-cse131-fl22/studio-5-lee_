2f941fffa55730756286c38af6ff866c06fe0d46
gracelee <gracelee@graces-mbp.dhcp.wustl.edu>
2022-10-19 15:46:26 -0500

    hi :)

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -14,7 +14,7 @@ public class Methods {
 	 * @return the Euclidean distance between (x1,y1) and (x2,y2)
 	 */
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
-		double distance = 0;
+		double distance = Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
 		
 		return distance;
@@ -34,17 +34,21 @@ public class Methods {
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
+		StdDraw.setPenColor(0, 109, 219);
+		StdDraw.filledCircle(x, y, radius*(3.0/4.0));
 
 		
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
-
+		StdDraw.setPenColor(146, 0, 0);
+		StdDraw.filledCircle(x, y, radius*(1.0/2.0));
 		
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
-
+		StdDraw.setPenColor(255, 255, 109);
+		StdDraw.filledCircle(x, y, radius*(1.0/4.0));
 		
 	}
 
@@ -62,7 +66,16 @@ public class Methods {
 	public static String substituteAll(String source, char target, String replacement) {
 		String result = "";
 		// TODO: Finish this method
-		
+		char[] sourceLetters = source.toCharArray();
+		for (int i = 0; i < source.length(); i++) {
+			Character char1 = source.charAt(i);
+			if (char1.equals(target)) {
+				result = result+replacement;
+			}
+			else {
+				result = result + sourceLetters[i];
+			}
+		}
 		return result;
 	}
 
@@ -75,7 +88,9 @@ public class Methods {
 	public static int arraySum(int[] values) {
 		int sum = 0;
 		// FIXME: Compute the sum of the values in an array
-		
+		for (int i = 0; i < values.length; i++) {
+			sum = sum + values[i];
+		}
 		return sum;
 	}
 
@@ -87,8 +102,9 @@ public class Methods {
 	 * @return and array of size that's filled with value
 	 */
 	public static int[] filledArray(int length, int value) {
-		int[] values = null; // FIXME: Create an array of the appropriate size
+		int[] values = new int[length]; // FIXME: Create an array of the appropriate size
 		// TODO: Finish this method
+		
 
 		
 
